212b77cad72edbf6f7dcb3de9c4939d3909ae85e
rmrjr <xxyoungchinoxx@gmail.com>
2016-04-21 07:39:45 -0400

    Added files via upload

--- mainLoop.java
+++ mainLoop.java
@@ -7,34 +7,40 @@ public class mainLoop {
              
      SpiConnector spi = new SpiConnector();
      Thresholds thresholds = new Thresholds();
-
-     //WriteFile w = new WriteFile(data.getFilePath()); 
-     Pause pause = new Pause();
+     WriteFile w = new WriteFile(); 
      MonitorGui display = new MonitorGui();
+     Pause pause = new Pause
      
      Timer t = new Timer();
      TimerTask mainLooper = new TimerTask(){
      //sets up following calls to be run as one task.
+        
+        w.setUserData(data.UserData); 
+        w.printHeader();
          
       public void run(){
-          
+ 
         spi.getSpiData(data.getNumberOfCells);
+        //passes gui data to spi.
 
         float[] cellData = spi.getSpiData(numCells);
+        
+        w.setCellData(cellData);
+        w.printData();
         
          for (int i=0); i<numCells; i++){
             thresholds.checkMinThresholds(userMinVoltage, i+1, cellData[i]);
                 if(thresholds.checkMinThresholds = false){
-                   pause.checkPause(); 
+                   pause.pauseLoop(); 
                 }//endif
             thresholds.checkMaxThresholds(userMaxVoltage, i+1, cellData[i]);
                 if(thresholds.checkMaxThresholds = false){
-                    pause.checkPause();
+                    pause.pauseLoop();
                 }//endif
             }//end for loop
         
-        display.Monitor.textBox.setText(String.parseText(cellData));
-      }
+        display.Monitor(cellData);
+      }// passes spi data to monitor gui.
         
      };
      
@@ -43,3 +49,4 @@ public class mainLoop {
    }
     
 }
+//end mainLoop
