2d8919748aedb548d6391ab10beb901aad8ead80
James Malafronte <malafronte-james@users.noreply.github.com>
2016-04-20 21:23:54 -0400

    Re-done TPIGUI class
    created all actions in the class and created a customButton class.

---
+++ CustomButton.java
@@ -0,0 +1,13 @@
+package GUI;
+
+import javax.swing.JButton;
+
+public class CustomButton extends JButton{
+
+	public CustomButton(String text, String toolTip)
+	{
+        setText(text);
+        setToolTipText (toolTip);
+	}
+	
+}
